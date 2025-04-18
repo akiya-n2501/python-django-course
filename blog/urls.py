528a4808e8678e8a9bb5798fcbb841cba9b0d0be
akiya-n2501 <keisei.akiya@hit-inc.jp>
2025-04-18 11:45:39 +0900

    ３-9終了

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from .views import (
     BlogCreateView,
+    BlogDeleteView,
     BlogDetailView,
     BlogEditView,
     BlogListView,
     IndexView,
     create_done,
+    delete_done,
     edit_done,
     index,
 )
@@ -19,7 +21,9 @@ urlpatterns = [
     path("", BlogListView.as_view(), name="blog_list"),
     path("create/", BlogCreateView.as_view(), name="create"),
     path("create_done/", create_done, name="create_done"),
-    path("detail/<int:pk>", BlogDetailView.as_view(), name="detail"),
-    path("edit/<int:pk>", BlogEditView.as_view(), name="edit"),
+    path("detail/<int:pk>/", BlogDetailView.as_view(), name="detail"),
+    path("edit/<int:pk>/", BlogEditView.as_view(), name="edit"),
     path("edit_done/", edit_done, name="edit_done"),
+    path("delete/<int:pk>/", BlogDeleteView.as_view(), name="delete"),
+    path("delete_done/", delete_done, name="delete_done"),
 ]
